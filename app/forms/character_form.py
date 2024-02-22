c5412d08af742c9a5c90273130ce7d180cf6fee4
Brian <bw49078@gmail.com>
2024-02-22 11:07:01 -0600

    fixed bug where user had to input their id to create a character

--- app/forms/character_form.py
+++ app/forms/character_form.py
@@ -6,7 +6,7 @@ from app.models import Character
 class CharacterForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired(), Length(min=1, max=40, message="Name must be between 1 and 40 characters")])
-    user_id = IntegerField('owner', validators=[DataRequired()])
+    user_id = IntegerField('owner')
     gender = StringField('gender', validators=[DataRequired(), Length(min=1, max=40, message="Gender must be between 1 and 40 characters")])
     age = IntegerField('age', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired(), Length(min=1, max=255, message="Description must be between 1 and 255 characters")])
