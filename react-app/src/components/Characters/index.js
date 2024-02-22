6fc9a9c773fce3d977fb5a8bb9dd90438abd9223
Brian <bw49078@gmail.com>
2024-02-22 15:26:09 -0600

    page now displays a list of characters made by current user

--- react-app/src/components/Characters/index.js
+++ react-app/src/components/Characters/index.js
@@ -5,9 +5,25 @@ import { useSelector } from 'react-redux';
 const UserCharacters = () => {
   const history = useHistory();
   const sessionUser = useSelector(state => state.session.user);
+  const characters = sessionUser.characters;
+
+  console.log(characters)
 
   return (
-    <h1>Hello {sessionUser.username}</h1>
+    <>
+        <h1>Hello {sessionUser.username}</h1>
+        {characters ? (
+            <div>
+                <ul>
+                    {characters.map(character => (
+                        <li key={character.id}>{character.name}</li>
+                    ))}
+                </ul>
+            </div>
+        ) : (
+            <h1>No Characters</h1>
+        )}
+    </>
   )
 }
 
