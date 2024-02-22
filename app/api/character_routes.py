9efeed7c0ded84c648c110c08b9d68e5732ddd28
Brian <bw49078@gmail.com>
2024-02-22 11:28:11 -0600

    user can now edit characters they created

--- app/api/character_routes.py
+++ app/api/character_routes.py
@@ -32,6 +32,42 @@ def create_character():
 
     if form.errors:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+    
+
+@character_routes.route('/edit/<int:id>', methods=['PUT'])
+@login_required
+def update_character(id):
+    """
+    Updates a character
+    """
+    character = Character.query.get(id)
+    user = current_user.to_dict()
+    form = CharacterForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        if character is None:
+            return {'errors': 'Character not found'}, 404
+        
+        character_dict = character.to_dict()
+
+        if user['id'] != character_dict['user_id']:
+            return {'errors': 'You do not have permission to edit this character'}, 403
+
+        if character.name != form.data['name']:
+            character.name = form.data['name']
+        if character.age != form.data['age']:
+            character.age = form.data['age']
+        if character.description != form.data['description']:  
+            character.description = form.data['description']
+        if character.gender != form.data['gender']:
+            character.gender = form.data['gender']
+        character.updated_at = datetime.now()
+        db.session.commit()
+        return character.to_dict()
+
+    if form.errors:
+        return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
 
 @character_routes.route('/delete/<int:id>', methods=['DELETE'])
