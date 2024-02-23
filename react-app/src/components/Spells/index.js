51c866f6c3b7d8cb6dac5bfaef87c0d6e73ba1ab
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-02-22 16:14:28 -0800

    added images for schools

--- react-app/src/components/Spells/index.js
+++ react-app/src/components/Spells/index.js
@@ -5,8 +5,13 @@ const Spells = () => {
 
   return (
     <div id='spells-page'>
-      <div id='spells-background'>
+      <div id='spell-filter-container'>
         <h1>Spells</h1>
+        <div id='spell-filter'>
+          <div id='filter-school-buttons'>
+            <span id='test'></span>
+          </div>
+        </div>
       </div>
     </div>
   )
