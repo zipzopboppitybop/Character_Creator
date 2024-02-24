083802e733579af6eb7885e09c26d451593db935
Brian <bw49078@gmail.com>
2024-02-24 14:02:40 -0600

    added extra columns to the character model

--- app/models/character.py
+++ app/models/character.py
@@ -9,9 +9,28 @@ class Character(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    name = db.Column(db.String(40), nullable=False)
+    name = db.Column(db.String(40))
     gender = db.Column(db.String(40))
+    race = db.Column(db.String(40))
+    class_one_name = db.Column(db.String(40))
+    class_two_name = db.Column(db.String(40))
+    class_three_name = db.Column(db.String(40))
+    class_four_name = db.Column(db.String(40))
+    class_five_name = db.Column(db.String(40))
+    class_six_name = db.Column(db.String(40))
+    class_seven_name = db.Column(db.String(40))
+    class_eight_name = db.Column(db.String(40))
+    class_nine_name = db.Column(db.String(40))
+    class_ten_name = db.Column(db.String(40))
+    class_eleven_name = db.Column(db.String(40))
+    class_twelve_name = db.Column(db.String(40))
     age = db.Column(db.Integer)
+    strength = db.Column(db.Integer)
+    dexterity = db.Column(db.Integer)
+    constitution = db.Column(db.Integer)
+    intelligence = db.Column(db.Integer)
+    wisdom = db.Column(db.Integer)
+    charisma = db.Column(db.Integer)
     description = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now())
@@ -26,6 +45,24 @@ class Character(db.Model):
             'gender': self.gender,
             'age': self.age,
             'description': self.description,
+            'strength': self.strength,
+            'dexterity': self.dexterity,
+            'constitution': self.constitution,
+            'intelligence': self.intelligence,
+            'wisdom': self.wisdom,
+            'charisma': self.charisma,
+            'class_one_name': self.class_one_name,
+            'class_two_name': self.class_two_name,
+            'class_three_name': self.class_three_name,
+            'class_four_name': self.class_four_name,
+            'class_five_name': self.class_five_name,
+            'class_six_name': self.class_six_name,
+            'class_seven_name': self.class_seven_name,
+            'class_eight_name': self.class_eight_name,
+            'class_nine_name': self.class_nine_name,
+            'class_ten_name': self.class_ten_name,
+            'class_eleven_name': self.class_eleven_name,
+            'class_twelve_name': self.class_twelve_name,
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
