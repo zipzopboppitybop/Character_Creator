629d4e9735d329e9b4970596f829ff02ad3a3d82
Brian <bw49078@gmail.com>
2024-02-25 00:52:27 -0600

    slightly styled race item and added modal and modal button to them

--- react-app/src/components/Characters/CharacterBuilder/Race/RaceItem/index.js
+++ react-app/src/components/Characters/CharacterBuilder/Race/RaceItem/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import OpenRaceModalButton from '../OpenRaceModalButton';
+import ChooseRaceForm from '../RaceModal';
 import './RaceItem.css';
 
 const RaceItem = ({race}) => {;
@@ -13,10 +15,15 @@ const RaceItem = ({race}) => {;
             <img className='race-icon' alt='DND_SCHOOL_ABJURATION' src='../../../dwarf.png' />
             <div className='race-stats'>
                 <h1 className='race-name'>{race.name}</h1>
+                <p className='race-tldr'> {race.tldr}</p>
+                <OpenRaceModalButton
+              buttonText={"Choose Race"}
+              onClick={(e) => e.preventDefault()} 
+              modalComponent={<ChooseRaceForm race={race} />}
+             />
             </div>
+
         </div>
-        
-        
     </>
   )
 }
---
+++ react-app/src/components/Characters/CharacterBuilder/Race/RaceModal/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+
+
+function ChooseRaceForm({ race }) {
+
+
+    return (
+        <>
+            <h1>hi</h1>
+        </>
+    );
+}
+
+export default ChooseRaceForm;
